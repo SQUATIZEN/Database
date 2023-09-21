c5488d9dada323c6959c44c5458179a5d7140c99
zainulabidiinn <zainulabidinn20@gmail.com>
2023-09-21 15:11:48 +0700

    first commit

---
+++ deteksiban.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS user (
+  id_user INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password_user VARCHAR(30) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS ban (
+  id_ban INT AUTO_INCREMENT PRIMARY KEY,
+  foto_ban VARCHAR(200) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS perbandingan_ban (
+  id_perbandingan_ban INT AUTO_INCREMENT PRIMARY KEY,
+  id_ban INT,
+  status_perbandingan_ban VARCHAR(100) NOT NULL,
+  FOREIGN KEY (id_ban) REFERENCES ban(id_ban) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS kondisi_ban (
+  id_kondisi_ban INT AUTO_INCREMENT PRIMARY KEY,
+  id_ban INT,
+  id_perbandingan_ban INT,
+  status_kondisi_ban VARCHAR(100) NOT NULL,
+  FOREIGN KEY (id_ban) REFERENCES ban(id_ban) ON DELETE CASCADE,
+  FOREIGN KEY (id_perbandingan_ban) REFERENCES perbandingan_ban(id_perbandingan_ban) ON DELETE CASCADE
+) ENGINE=InnoDB;
