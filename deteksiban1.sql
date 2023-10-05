81d95c6ee0f066555618a6eb7ebe7b6dc7a7f37d
zainulabidiinn <zainulabidinn20@gmail.com>
2023-10-05 14:18:20 +0700

    commit 3

--- deteksiban1.sql
+++ deteksiban1.sql
@@ -2,7 +2,7 @@ import mysql.connector
 import cv2
 import numpy as np
 
-#Koneksikedatabase
+#KoneksiKeDatabase
 conn = mysql.connector.connect(
     host="mariadb",
     user="root",
@@ -11,29 +11,29 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-# Menyimpandatabandanhasilpengukurannyakedalamdatabase
+# MenyimpanDataBanDanHasilPengukurannyaKedalamDatabase
 sql = "INSERT INTO Ban (id_ban, id_user, foto_ban) VALUES (%s, %s, %s)"
 values = (id_ban, id_user, foto_ban)
 cursor.execute(sql, values)
 conn.commit()
 
-#Mendapatkanindikatortingkatkeausandaridatabase
-indicator_sql = "SELECT tread_depth_indicator FROM TreadDepthIndicator"
+#MendapatkanIndikatorTingkatKeausanDariDatabase
+indicator_sql = "SELECT indikatoryangdipake FROM indikatoryangdipake"
 cursor.execute(indicator_sql)
 indicators = cursor.fetchall()
 
-#Membandingkanhasilpengukurandenganindikatortingkatkeausan
+#MembandingkanHasilPengukurandDenganIndikatorTingkatKeausan
 result = ""
 for indicator in indicators:
     indicator_value = indicator[0]
-    if tread_depth >= indicator_value:
+    if hasilPengukuran >= indicator_value:
         result = "Ban Aus"
         break
     else:
-        result = "Ban Baik"
+        result = "Ban Bagus"
 
-# Menampilkan hasil pengukuran
-print("Hasil pengukuran ban:", tread_depth)
+# MenampilkanHasilPengukuran
+print("Hasil pengukuran ban:", hasilPengukuran)
 print("Status ban:", result)
 
 # Menutup koneksi
